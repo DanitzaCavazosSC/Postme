63d6c1fc3e31de3aba56841288e102cfbc8a8b24
Danitza Cavazos <20183l301033@utcv.edu.mx>
2022-11-05 09:46:31 -0600

    Intento_dami3

--- sw.js
+++ sw.js
@@ -1,18 +1,37 @@
 self.addEventListener('install', (event) => {
     //Nosotros deberiamos agregar al cache nuestros archivos
     console.info('[SW]: Adicionando nuestros archivos al cache...');
+    
+    const wu = new Promise((resolve, reject) => {
+        try {
+            setTimeout(() =>{
+            const adicionandoMisrchivo = '';
+            const adicioandoVariabesDonaamicas = '';
+            console.warn('[SW]: Se Instalo Correctmente...');
+            resolve();
+        }, 1000); //1 segundo
+            self.skipWaiting();
+            } catch (error){
+                reject(error.message);
+            }
+        })
+    event.waitUntil(wu);
+    });
 
-    setTimeout(() => {
-        console.warn('[SW]: Se instalo correctamente...');
-    }, 1000); //1 segundo
-    self.skipWaiting();
-});
 
 self.addEventListener('activate', (event) => {
+//la ddocumentecion nos indica elimiar todoss los cache anteriores
     console.info('[SW]: Archivos exitosamente guardados...');
     //event.waitUntil(clients.cliam());
 });
 
 self.addEventListener('fetch', (event) => {
     console.info('[SW]: Instalando...');
-})
+});
+
+self.addEventListener('sync', (event) => {
+    console.log('--------------------------------');
+    console.log(event);
+    console.log('----------------------------------');
+
+});
